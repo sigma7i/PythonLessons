7739913b4960fad502862b03fb5d59ceb345e720
maxim <zinatylin@gmail.com>
2018-03-08 17:50:20 +0300

    lesson03 home work

--- Lesson03/hard.py
+++ Lesson03/hard.py
@@ -1,24 +1,60 @@
 # Задание - 1
-# Давайте опишем пару сущностей player и enemy через словарь,
-# который будет иметь ключи и значения:
-# name - строка полученная от пользователя,
-# health - 100,
-# damage - 50.
-# Поэксперементируйте с значениями урона и жизней по желанию.
-# Теперь надо создать функцию attack(person1, persoтn2), аргументы можете указать свои,
-# функция в качестве аргумента будет принимать атакующего и атакуемого,
-# функция должна получить параметр damage атакующего и отнять это количество
-# health от атакуемого. Функция должна сама работать с словарями и изменять их значения.
-
-
 # Задание - 2
-# Давайте усложним предыдущее задание, измените сущности, добавив новый параметр - armor = 1.2
-# Теперь надо добавить функцию, которая будет вычислять и возвращать полученный урон по формуле damage / armor
-# Следовательно у вас должно быть 2 функции, одна наносит урон, вторая вычисляет урон по отношению к броне.
+def create_unit(name, health =100, damage = 50, armor = 1.2):
+    return {'name': name, 'health': health, 'damage': damage, 'armor': armor}
 
-# Сохраните эти сущности, полностью, каждую в свой файл,
-# в качестве названия для файла использовать name, расширение .txt
-# Напишите функцию, которая будет считывать файл игрока и его врага, получать оттуда данные, и записывать их в словари,
-# после чего происходит запуск игровой сессии, где сущностям поочередно наносится урон,
-# пока у одного из них health не станет меньше или равен 0.
-# После чего на экран должно быть выведено имя победителя, и количество оставшихся единиц здоровья.
+def get_damage(person, attack_person):
+    return attack_person['damage'] / person['armor']
+
+def attack(person, attack_person):
+    damage = get_damage(person, attack_person)
+    person['health'] -= damage
+    print('{} атакует {}'.format(attack_person['name'], person['name']))
+    print(f'Получен урон: {damage}')
+
+def save_to_file(persion):
+    with open(persion['name']+'.txt', 'w', encoding='UTF-8') as fw:
+        for kw, val in persion.items():
+            fw.write(f'{kw} {val}\n')
+
+def save_many_payers(*persons):
+    for p in persons:
+        save_to_file(p)
+
+def read_from_file(person):
+    persion_dic = {'name': person['name']}
+    with open(person['name'] + '.txt', 'r', encoding='UTF-8') as fr:
+        file_lines = fr.readlines()
+        for line in file_lines:
+            key, value = line.replace('\n', '').split(' ')
+            if key != 'name':
+                persion_dic[key] = float(value)
+
+    return create_unit(**persion_dic)
+
+def fight(player, enemy):
+    i = 1
+    while True:
+        if i % 2 == 0:
+            attack(player, enemy)
+        else:
+            attack(enemy, player)
+
+        if player['health'] <= 0:
+            print(player['name'] + ' проиграл')
+            break
+        if enemy['health'] <= 0:
+            print(enemy['name'] + ' проиграл')
+            break
+        i+=1
+
+# использование
+player = create_unit('elf', armor=1.3)
+enemy = create_unit('ork', 150, 40)
+
+save_many_payers(player, enemy)
+
+saved_player = read_from_file(player)
+saved_enemy = read_from_file(enemy)
+
+fight(saved_player, saved_enemy)
