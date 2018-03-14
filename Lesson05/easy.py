9eaca0a864318b8ecbb569f07169373bd87ff770
ZinatulinMR <ZinatulinMR@incbt.ru>
2018-03-14 14:23:00 +0300

    home work normal

--- Lesson05/easy.py
+++ Lesson05/easy.py
@@ -6,25 +6,38 @@ import os
 
 
 def create_dirs():
+    """Создает дитектории dir_1 - dir_9 в папке"""
     for i in range(1, 10):
         directory = 'dir_{}'.format(i)
-        if not os.path.exists(directory):
-            try:
-                os.mkdir(directory)
-            except PermissionError:
-                print('Отказано в доступе')
+        create_dir(directory)
+
+
+def create_dir(directory):
+    if not os.path.exists(directory):
+        try:
+            os.mkdir(directory)
+            return True
+        except PermissionError:
+            print('Отказано в доступе')
+    return False
 
 
 def delete_dirs():
+    """Удаляет директории dir_1 - dir_9"""
     for i in range(1, 10):
-        try:
-            os.rmdir('dir_{}'.format(i))
-        except PermissionError:
-            print('Отказано в доступе')
-        except FileNotFoundError:
-            print('Не найдена директория dir_{}'.format(i))
-        except OSError:
-            print('Папка не пуста dir_{}'.format(i))
+        delete_dir('dir_{}'.format(i))
+
+def delete_dir(directory):
+    try:
+        os.rmdir(directory)
+        return True
+    except PermissionError:
+        print('Отказано в доступе')
+    except FileNotFoundError:
+        print('Не найдена директория dir_{}'.format(directory))
+    except OSError:
+        print('Папка не пуста dir_{}'.format(directory))
+    return False
 
 
 if __name__ == '__main__':
@@ -35,6 +48,7 @@ if __name__ == '__main__':
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 def show_dirs():
+    """Отображает папки текущей директории."""
     for dir in os.listdir(os.getcwd()):
         if os.path.isdir(dir):
             print(dir)
@@ -43,16 +57,41 @@ def show_dirs():
 if __name__ == '__main__':
     show_dirs()
 
+
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
-import sys
-
 
 def copy_current_file():
-    with open(sys.argv[0], encoding='UTF-8') as f:
-        with open('copy_' + os.path.basename(sys.argv[0]), 'w', encoding='UTF-8') as w:
+    """Создает копию файла, из которого запущен"""
+    with open(__file__, encoding='UTF-8') as f:
+        with open('copy_' + os.path.basename(__file__), 'w', encoding='UTF-8') as w:
             w.writelines(f.readlines())
+
+
+def delete_copyes():
+    """Удаляет созданные копии функцией copy_current_file"""
+    files_to_del = [d for d in os.listdir(os.getcwd()) if os.path.isfile(d) and d.startswith('copy_')]
+    os.remove(*files_to_del)
 
 
 if __name__ == '__main__':
     copy_current_file()
+    delete_copyes()
+
+# Для задания normal
+
+def directory_action(action, ok ='успешно', failure = 'Ошибка', log = None):
+    '''Действия над директорией - для задания normal'''
+    directory = input('введите назнвание директории')
+    path = os.path.join(os.getcwd(), directory)
+    if action(path):
+        print(ok)
+        if log != None: log(path)
+    else:
+        print(failure)
+
+def change_directory():
+    directory = input('введите назнвание директории')
+    os.chdir(directory)
+    print(f'Вы находитесь: {os.getcwd()}')
+
--- Lesson05/normal.py
+++ Lesson05/normal.py
@@ -13,3 +13,46 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+
+import Lesson05.easy as easy
+import os
+
+CONSOLE_TEXT = '''
+1. Перейти в папку
+2. Просмотреть содержимое текущей папки
+3. Удалить папку
+4. Создать папку
+5. Вернуть все как было
+6. Выход
+'''
+print(os.getcwd())
+
+# сохраняем возможность вернуть все на место
+actions_list = []
+
+while True:
+    try:
+        action = int(input(CONSOLE_TEXT))
+    except ValueError:
+        print('Необходимо выбрать пункт')
+
+    if action == 1:
+        easy.change_directory()
+    elif action == 2:
+        easy.show_dirs()
+    elif action == 3:
+        easy.directory_action(easy.delete_dir,
+                              ok='Успешно удалено',
+                              failure='Ошибка удаления',
+                              log=actions_list.remove)
+    elif action == 4:
+        easy.directory_action(easy.create_dir,
+                              ok='Успешно создано',
+                              failure='Ошибка создания',
+                              log=actions_list.append)
+    elif action == 5:
+        for d in actions_list: easy.delete_dir(d)
+    elif action == 6:
+        break
+    else:
+        print('Неизвестная команда')
